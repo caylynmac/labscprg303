0831d2ed312d69cefda5691a28338befddd43b06
caylyn <152448716+caylynmac@users.noreply.github.com>
2025-01-23 16:59:12 -0700

    fruitlist

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import ButtonTemplate from "..//components/button_template";
+import FruitList from "..//components//fruit";
 
 export default function App() {
     return (
         <View>
             <Text>Welcome to Lab 2!</Text>
             <ButtonTemplate link={"/"} text={"Tabs"} color="green" />
+            <FruitList/>
         </View>
     )
 }
