10ecba3bd29d637c7316fa5db4c6b16fa096e254
BrodyWilson97 <153255880+BrodyWilson97@users.noreply.github.com>
2025-02-13 16:31:42 -0700

    vacationDestinations

--- app/lab4.tsx
+++ app/lab4.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
 import { useRouter } from "expo-router";
 import { styles } from "../styles";
+import vacationDestinations from "../constants/list_items";
 
 const Lab4 = () => {
 
